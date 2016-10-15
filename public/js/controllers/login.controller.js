b5bfdbce75e4254d1071067da3533bdf3f21a0ab
Jory Schmidt <joryschmidt1988@gmail.com>
2016-10-15 01:04:33 +0000

    added login functionality

--- public/js/controllers/login.controller.js
+++ public/js/controllers/login.controller.js
@@ -1,8 +1,26 @@
 (function() {
   'use strict';
   
-  angular.module('app').controller('LoginController', [function() {
-    
+  angular.module('app')
+  .controller('LoginController', ['$scope', '$state', '$cookies', 'Auth', function($scope, $state, $cookies, Auth) {
+    $scope.error = false;
+    $scope.login = function() {
+      var user = {
+        username: $scope.username,
+        password: $scope.password
+      };
+      
+      Auth.login(user).success(function(data) {
+        console.log('Login successful');
+        $cookies.put('user', data.user.username);
+        $cookies.put('userId', data.user._id);
+        $state.go('add');
+      }).error(function() {
+        console.log('Error logging in');
+        $scope.error = true;
+        $scope.errorMessage = 'There was a problem logging in, mate';
+      });
+    };
   }]);
   
 })();
